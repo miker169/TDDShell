260529884ad08993830c34d4adc23e0bbb03470e
Mike Rayner <miker169@hotmail.co.uk>
2013-05-03 11:27:15 +0100

    Added further tests for a game up to 40 points

--- Tests/unitTests.sh
+++ Tests/unitTests.sh
@@ -17,5 +17,33 @@ function testItCanGetScoreForAPlayerWithOnlyOneWin() {
   assertEquals '1' `getScoreFor 'John' "$standings"`
 }
 
+function testItCanGetAScoreForAPlayer() {
+  standings=$'John - Michael\nJohn\nMichael\nJohn'
+  assertEquals '2' `getScoreFor 'John' "$standings"`
+}
+
+function testItCanOutputScoreAsInTennisForFirstPoint() {
+  assertEquals 'John: 15 - Michael: 0' "`displayScore 'John' 1 'Michael' 0`"
+}
+
+function testItCanOutputScoreAsInTennisForSecondPointFirstPlayer () {
+  assertEquals 'John: 30 - Michael: 0' "`displayScore 'John' 2 'Michael' 0`"
+}
+
+function testItCanOutputScoreAsInTennisForTHIRDPointFirstPlayer() {
+  assertEquals 'John: 40 - Michael: 0' "`displayScore 'John' 3 'Michael' 0`"
+}
+function testItCanOutputScoreWhenFirstPlayersWinsFirst3Points() {
+  assertEquals 'John: 15 - Michael: 0' "`displayScore 'John' 1 'Michael' 0`"
+  assertEquals 'John: 30 - Michael: 0' "`displayScore 'John' 2 'Michael' 0`"
+  assertEquals 'John: 40 - Michael: 0' "`displayScore 'John' 3 'Michael' 0`"
+}
+
+function testItCanOutputScoreWhenSecondPlayerWinsFirst3Points() {
+  assertEquals 'John: 0 - Michael: 15' "`displayScore 'John' 0 'Michael' 1`"
+  assertEquals 'John: 0 - Michael: 30' "`displayScore 'John' 0 'Michael' 2`"
+  assertEquals 'John: 0 - Michael: 40' "`displayScore 'John' 0 'Michael' 3`"
+}
+
 ### Call and Run all Tests
 . "../shunit2-2.1.6/src/shunit2"
--- functions.sh
+++ functions.sh
@@ -14,3 +14,22 @@ function getScoreFor () {
   totalMatches=$(echo "$standings" | grep $player | wc -l)
   echo $(($totalMatches-1))
 }
+
+function displayScore () {
+  echo "$1: `convertToTennisScore $2` - $3: `convertToTennisScore $4`"
+}
+
+function convertToTennisScore () {
+  if [ "$1" -eq '1' ]; then
+    playerOneScore='15'
+  elif [ "$1" -eq '2' ]; then
+    playerOneScore='30'
+  elif [ "$1" -eq '3' ]; then
+    playerOneScore='40'
+  else
+    playerOneScore=$1
+  fi
+
+  echo $playerOneScore;
+}
+
