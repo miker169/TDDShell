03779b4fd2c7b31c3beb6a30d1672f15f2b3df89
Mike Rayner <miker169@hotmail.co.uk>
2013-05-03 11:03:16 +0100

    Added function for getting score for a player

--- functions.sh
+++ functions.sh
@@ -7,3 +7,10 @@ function getFirstPlayerFrom () {
 function getSecondPlayerFrom () {
   echo $1 | sed -e 's/.*-//'
 }
+
+function getScoreFor () {
+  player=$1
+  standings=$2
+  totalMatches=$(echo "$standings" | grep $player | wc -l)
+  echo $(($totalMatches-1))
+}
